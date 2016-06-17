651125f947dd56791d7cfc0b3fb65cdc6570aeef
nikhilhassija <nikhil.hassija@gmail.com>
2016-06-17 18:02:42 +0530

    Changed Add and Issue views. Using tables for issued medicines

--- myapp/static/myapp/js/script.js
+++ myapp/static/myapp/js/script.js
@@ -8,16 +8,24 @@ jQuery(document).ready(function($)
 
     $("#add").click(function()
     {
-    	n = parseInt($('#max_n').val());
-    	s = $("#form-group-"+1).html();
-    	var m = new RegExp("medicine"+1,"g");
-    	s = s.replace(m,"medicine"+n);
-    	var q = new RegExp("quantity"+1,"g");
-    	s = s.replace(q,"quantity"+n);
-    	s = s.replace(/dropdown-toggle/g,"dropdown-toggle disabled");
-    	s = "<div class=\"form-group\" id=\"form-group-" + n + "\">" + s + "</div>";
-    	$(s).insertAfter("#form-group-"+1);
+        // s = $("#form-group-"+1).html();
+        // var m = new RegExp("medicine"+1,"g");
+        // s = s.replace(m,"medicine"+n);
+        // var q = new RegExp("quantity"+1,"g");
+        // s = s.replace(q,"quantity"+n);
+        // s = s.replace(/dropdown-toggle/g,"dropdown-toggle disabled");
+        // s = "<div class=\"form-group\" id=\"form-group-" + n + "\">" + s + "</div>";
+        // $(s).insertAfter("#form-group-"+1);
+    	
+        n = parseInt($('#max_n').val());
+        $('#max_n').val(n+1);
+
+        s = "<tr><td>"+n+"</td><td>"+$("#medicine1").val()+"</td><td>"+$("#quantity1").val()+"</td></tr>";
+        $('.table').append(s)
+
     	str = str + $("#quantity1").val() + ',' + $("#medicine1").val() + '|'; 
     	$("#str").attr("value",str);
+
+
     });
 });
