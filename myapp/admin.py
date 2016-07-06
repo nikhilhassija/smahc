20997065e69adfc6f49a263ae03ccbe74c097fb4
nikhilhassija <nikhil.hassija@gmail.com>
2016-07-06 18:49:25 +0530

    CHANGES

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Medicine, Log, GLog
+from .models import Medicine, Log
 
 # Register your models here.
 
 admin.site.register(Medicine)
 admin.site.register(Log)
-admin.site.register(GLog)
